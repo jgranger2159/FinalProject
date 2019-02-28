b7bfd37a5724ca058d936916de4084d47c4b729e
Perry the Platypus <jgranger2159@gmail.com>
2019-02-27 18:49:25 -0700

    all done!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import nltk
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
 from future.utils import iteritems
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request
 import pandas as pd
 
 app = Flask(__name__)
@@ -41,33 +41,48 @@ def home():
     return render_template("template.html")
 
 
-@app.route('/click')
-def received(review):
-    return redirect('/')
-
-    # value = format_review(review)
-    # print(review_score)
+@app.route('/click', methods=['GET', 'POST'])
+def received():
+    if request.method == 'POST':
+        review = request.form['review']
+        value = format_review(review)
+        print(review)
         
-    # if value == 0:
-    #     return "I don't know these words, sorry"
-    # elif value > 8:
-    #     return 5
-    # elif value > 0:
-    #     return 4
-    # elif value > -20:
-    #     return 2
-    # else:
-    #     return 1
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        if value == 0:
+            return redirect('/unknown')
+        elif value > 8:
+            return redirect('/five')
+        elif value > 0:
+            return redirect('/four')
+        elif value > -20:
+            return redirect('/two')
+        else:
+            return redirect('/one')
+    else:
+        review = request.args.get('review')
 
+    return redirect('/')
+    
+@app.route('/unknown')
+def unknown():
+    return render_template('template_unknown.html')
+
+@app.route('/five')
+def five():
+    return render_template("template_five.html")
+
+@app.route('/four')
+def four():
+    return render_template("template_four.html")
+
+@app.route('/two')
+def two():
+    return render_template("template_two.html")
+
+@app.route('/one')
+def one():
+    return render_template("template_one.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
